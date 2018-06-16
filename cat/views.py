fc9dbd9adb30658450f7bdbcf67b06bca4f7d73b
Ryosuke Imai <ryosukelly.slater@gmail.com>
2018-06-16 17:15:42 +0900

    modeladd登録

--- cat/views.py
+++ cat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #from django.http import HttpResponse
 from .forms import CatCreateForm
 
@@ -30,7 +30,21 @@ def catlist(request):
     return render(request, 'cat/cat_list.html', context)
 
 def catadd(request):
-    context = {
-        'form':CatCreateForm()
-    }
-    return render(request, 'cat/cat_add.html', context)
+#    context = {
+#        'form':CatCreateForm()
+#    }
+#    return render(request, 'cat/cat_add.html', context)
+    # 送信内容を元にフォームを作る　POSTじゃなければ空フォーム
+    form = CatCreateForm(request.POST or None)
+
+    # method=POST つまり送信ボタン押下時入力内容が問題なければ
+    if request.method=='POST' and form_is_valid():
+        form_save()
+        return redirect(cat:catlist)
+
+        # 通常時のページアクセスや入力内容に誤りがあった場合
+        context = {
+            'form':form
+        }
+    return render(request,'cat/cat_add.html'.context)
+
