ab11b15de1b5ea37d37e367844a8d5878250d061
Ryosuke Imai <ryosukelly.slater@gmail.com>
2018-06-16 16:07:43 +0900

    ２HTMM

--- cat/views.py
+++ cat/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 def index(request):
 #    return HttpResponse("Hellow, world")
 #    return render(request, 'cat/index.html')
+    """/トップページ"""
     context = {
         'name':'RyosukellyPi',
     }
     return render(request, 'cat/index.html', context)
+
+def about(request):
+    """/about"""
+    return render(request, 'cat/about.html')
+
+def info(request):
+    """/info"""
+    return render(request, 'cat/info.html')
